acbcbc50d4592948fc6ec509bfdce8fce1bffaf9
Jorge Arias Argüelles <69608367+jorgearguellles@users.noreply.github.com>
2025-02-19 15:22:26 -0500

    Update hola_lenguaje.py

--- hola_lenguaje.py
+++ hola_lenguaje.py
@@ -1,10 +1,9 @@
 import os
 
 def main():
-    nombre = os.getenv('USERNAME')
-    lenguaje = os.getenv('LANGUAGE')
+    nombre = os.getenv("USERNAME")
     
-    print(f"¡Hola, {nombre}! Veo que tu lenguaje favorito es {lenguaje}.")
+    print(f"¡Hola, {nombre}! Veo que tu lenguaje favorito es JavaScript.")
 
 if __name__ == "__main__":
     main()
